cbcdbee70972d7caf2d1e5bc6a6de96cd1bb3c98
Andrew <128154500+Andrey-Shelikhanov@users.noreply.github.com>
2023-06-25 23:27:31 +0300

    initial commit

---
+++ src/test/java/ru/netology/postmanEcho/PostmanEchoTest.java
@@ -0,0 +1,12 @@
+package ru.netology.postmanEcho;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+
+}
+
