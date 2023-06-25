c98460fecd473d7c3e6d25087a415c702a599b9e
Andrew <128154500+Andrey-Shelikhanov@users.noreply.github.com>
2023-06-25 23:33:58 +0300

    the build is failed

--- src/test/java/ru/netology/postmanEcho/PostmanEchoTest.java
+++ src/test/java/ru/netology/postmanEcho/PostmanEchoTest.java
@@ -6,6 +6,18 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
+    @Test
+    void shouldReturnSameData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello world!")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello"))
+        ;
+    }
 
 
 }
